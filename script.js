15fbd2b945a3ba9097841e387739d6aba4e3c9e3
EvelynGonzalez21 <115804333+EvelynGonzalez21@users.noreply.github.com>
2023-04-19 19:05:54 +0000

    🏗Running Draggin Function

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-//Tomado el control del objeto
-let plant1 = document.getElementById("plant1")
-//agregar
-plant1.addEventListener("click", function(){
-    let name = prompt("Hola, ¿Cual es tu nombre?");
-    if(name !==""){
-    alert(`${name} please, save the planet🌻`);
+// Haciendo que la planta 1 de mi documento
+// sea arrastable
+dragElement(document.getElementById("plant1"));
+// Implementadno la funcion drag elemnt
+ function dragElement (terrariumElement){
+    //Creando variables para el control de las posiciones 
+    //Las posiciones iniales y finales
+    let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0
+    // Registrar un evento
+    terrariumElement.onpointerdown = pointerDrag
+    
+    function pointerDrag( event ){
+        //Previene cualquier elemento  que tenga por defecto
+        // el evento en cuestion en mi documento
+        event.preventDefault();
+        console.log("+ On pointer down");
     }
-});
+}
